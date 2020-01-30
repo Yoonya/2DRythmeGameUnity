3bebf5e5cb02b8f6ec7310516363bfa5179776f3
todtygod2 <51391293+todtygod2@users.noreply.github.com>
2020-01-30 15:07:25 +0900

    구분선 작업 및 노트 오브젝트 생성과 이동

---
+++ Unity 2D Rythme Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //싱글 톤 처리
+    public static GameManager instance { get; set; }
+    private void Awake()
+    {
+        if (instance == null) instance = this;
+        else if (instance != this) Destroy(gameObject);
+
+    }
+
+    public float noteSpeed; //노트 스피드는 모두 공통이므로 여기서 관리
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
